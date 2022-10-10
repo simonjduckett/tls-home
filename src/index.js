a549f2af17298df865f7e3a7f3c5c118ffe04398
simonduckett <simonjduckett@hotmail.co.uk>
2022-10-10 15:32:12 +0100

    more update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ window.onload = (event) => {
         let experimentContent = document.createElement('div')
         experimentContent.classList.add('rwd-bs')
 
-        //create hero
         experimentContent.appendChild(createHero())
         experimentContent.appendChild(createTopCards())
         experimentContent.appendChild(createCategories())
@@ -13,12 +12,15 @@ window.onload = (event) => {
         experimentContent.appendChild(createWhoSection())
         experimentContent.appendChild(createReviewsSection())
         experimentContent.appendChild(createLogosSection())
-        experimentContent.appendChild(buildNesting([
-            { el: 'div', classname: 'container' },
-            { el: 'div', classname: 'row' },
-            { el: 'div', classname: 'col-12' },
-            { el: 'div', classname: 'col-12', sib: true }
-        ]))
+        experimentContent.appendChild(createTopFooter())
+
+
+        // experimentContent.appendChild(buildNesting([
+        //     { el: 'div', classname: 'container' },
+        //     { el: 'div', classname: 'row' },
+        //     { el: 'div', classname: 'col-12' },
+        //     { el: 'div', classname: 'col-12', sib: true }
+        // ]))
         
         let header = document.querySelector('header')
         header.after(experimentContent)
@@ -27,9 +29,14 @@ window.onload = (event) => {
 
         new Glide('.glide', {
             type: 'carousel',
-            //autoplay: 3000,
+            autoplay: 3000,
             perView: 4,
-            gap: 25
+            gap: 25,
+            breakpoints: {
+                768: {
+                    perView: 1
+                }
+            }
         }).mount()
     }
 
@@ -39,7 +46,7 @@ window.onload = (event) => {
         let c = createContainer()
         c.classList.add('heroContainer')
 
-        c.innerHTML += '<div><h1>Headline Text</h1><p>intro text intro text intro text intro text intro text</p><div class="trusted"><i class="fa-solid fa-circle-check"></i><p>Trusted by over 50,000 customers who used us to get legal quotes</p></div></div>'
+        c.innerHTML += '<div><h1>The legal comparison website</h1><p>Helping you save money on legal bills</p><div class="trusted"><i class="fa-solid fa-circle-check"></i><p>Trusted by over 50,000 customers</p></div></div>'
 
         hero.appendChild(c)
         return hero
@@ -50,7 +57,7 @@ window.onload = (event) => {
         topCards.id = 'topCards'
         topCards.classList.add('py-3')
         let c = createContainer()
-        let h = document.createElement('h3')
+        let h = document.createElement('h2')
         h.classList.add('mb-2')
         h.innerHTML = 'Compare and get legal quotes'
         c.appendChild(h)
@@ -105,7 +112,7 @@ window.onload = (event) => {
             let card = document.createElement('div')
             card.classList.add('top-card')
 
-            card.innerHTML = `<strong>${this.title}</strong><img src='${this.img}' /><p>${this.content}</p><a class='btn btn--orange' href='${this.link}'>${this.title}</a>`
+            card.innerHTML = `<h3>${this.title}</h3><img src='${this.img}' /><p>${this.content}</p><a class='btn btn--orange' href='${this.link}'>${this.title}</a>`
 
             return card
         }
@@ -221,12 +228,12 @@ window.onload = (event) => {
 
         //add everything to c1 and then append to row
         let h = document.createElement('h2')
-        h.innerHTML = 'How The Law Superstore Works'
+        h.innerHTML = 'Compare lawyers in three simple steps'
         c1.appendChild(h)
 
-        c1.appendChild(new point({ icon: '<i class="fa-solid fa-1"></i>', text: 'blah blah blah'}))
-        c1.appendChild(new point({ icon: '<i class="fa-solid fa-2"></i>', text: 'blah blah blah'}))
-        c1.appendChild(new point({ icon: '<i class="fa-solid fa-3"></i>', text: 'blah blah blah'}))
+        c1.appendChild(new point({ icon: '<i class="fa-solid fa-1"></i>', text: 'Pick the legal service you need.'}))
+        c1.appendChild(new point({ icon: '<i class="fa-solid fa-2"></i>', text: 'Fill in a few details on our quick quote form.'}))
+        c1.appendChild(new point({ icon: '<i class="fa-solid fa-3"></i>', text: 'Get instant quotes to your inbox.'}))
 
         function point(data) {
             this.icon = data.icon
@@ -265,28 +272,28 @@ window.onload = (event) => {
 
         //add everything to c1 and then append to row
         let h = document.createElement('h2')
-        h.innerHTML = 'Who Are The Law Superstore?'
+        h.innerHTML = 'Who is The Law Superstore?'
         c1.appendChild(h)
 
         let leftPara = document.createElement('p')
-        leftPara.innerHTML = "The Law Superstore is an independent and impartial comparison website for legal services. We\'ve made it our business to make finding legal services as simple and transparent as possible."
+        leftPara.innerHTML = "The Law Superstore is the UK\’s biggest legal comparison website. We\’re making it quick and easy to find lawyers. <br /> </br />With over 130 legal services on offer, from divorce to personal injury claims, we\’ve got you covered."
         c1.appendChild(leftPara)
 
         let checkList = document.createElement('ul')
 
-        checkList.appendChild(new checkPoint('Compare the best'))
-        checkList.appendChild(new checkPoint('<b>Instant results</b>, compare on what matters to you'))
-        checkList.appendChild(new checkPoint('Quality checked legal service providers'))
-        checkList.appendChild(new checkPoint('Hear from your chosen provider within 4 hours'))
-        checkList.appendChild(new checkPoint('Only pay when you\'ve spoken and agree terms'))
-        checkList.appendChild(new checkPoint('Prices available for initial advice only and full service'))
+        checkList.appendChild(new checkPoint('Find the lawyer who works best for you'))
+        checkList.appendChild(new checkPoint('<b>Save time</b> by getting instant quotes instead of going from provider to provider'))
+        checkList.appendChild(new checkPoint('Get the best deal by finding the cheapest lawyer'))
+        //checkList.appendChild(new checkPoint('Hear from your chosen provider within 4 hours'))
+        //checkList.appendChild(new checkPoint('Only pay when you\'ve spoken and agree terms'))
+        //checkList.appendChild(new checkPoint('Prices available for initial advice only and full service'))
 
         c1.appendChild(checkList)
 
         function checkPoint(text) {
             this.text = text
             let x = document.createElement('li')
-            x.innerHTML = this.text
+            x.innerHTML = `<div>${this.text}</div>`
             return x
         }
 
@@ -295,9 +302,11 @@ window.onload = (event) => {
         let c2 = document.createElement('div')
         c2.classList.add('col-12', 'col-md-6')
 
-        c2.appendChild(new boldPoint({ icon: '<i class="fa-solid fa-certificate"></i>',title: 'Trusted Partners', text: 'Quality checked and fully regulated or accredited legal service providers' }))
-        c2.appendChild(new boldPoint({ icon: '<i class="fa-solid fa-minimize"></i>',title: 'Compare Instantly', text: 'Quality checked and fully regulated or accredited legal service providers' }))
-        c2.appendChild(new boldPoint({ icon: '<i class="fa-solid fa-sterling-sign"></i>',title: 'Flexible Pricing', text: 'Quality checked and fully regulated or accredited legal service providers' }))
+        c2.appendChild(new boldPoint({ icon: '<i class="fa-solid fa-certificate"></i>', title: 'Find expert lawyers', text: 'All our legal service providers are regulated, accredited, and strictly vetted.' }))
+
+        c2.appendChild(new boldPoint({ icon: '<i class="fa-solid fa-minimize"></i>', title: 'Get instant quotes', text: 'Start comparing lawyers in 30 seconds.' }))
+
+        c2.appendChild(new boldPoint({ icon: '<i class="fa-solid fa-sterling-sign"></i>', title: 'Save money', text: 'Find the cheapest deal by comparing legal service providers.' }))
 
         //add everything to c2 and then append to row
         function boldPoint(data) {
@@ -311,10 +320,6 @@ window.onload = (event) => {
             return x
         }
 
-
-        //c2.innerHTML = 'hfdshdshdshfh'
-        
-
         row.appendChild(c2)
         c.appendChild(row)
         
@@ -327,6 +332,12 @@ window.onload = (event) => {
         reviewsSection.id = 'tls-reviews'
         let con = createContainer()
 
+        let heading = document.createElement('h2')
+        heading.classList.add('mb-2')
+        heading.innerHTML = 'What our clients say'
+
+        con.appendChild(heading)
+
         let glide = document.createElement('div')
         glide.classList.add('glide')
         let glideTrack = document.createElement('div')
@@ -338,15 +349,33 @@ window.onload = (event) => {
         glideTrack.appendChild(glideUl)
         glide.appendChild(glideTrack)
 
-
-
         for(let i = 0; i < slides.length; i++) {
             let x = document.createElement('li')
             x.innerHTML = slides[i].innerHTML
             x.classList.add('glide__slide')
+            let z = x.querySelector('.testimonial__blockquote')
+            let y = document.createElement('div')
+            let j = 0
+            while(j < 5) {
+                y.innerHTML += '<i class="fa-solid fa-star"></i>'
+                j++
+            }
+            z.appendChild(y)
             glideUl.appendChild(x)
         }
 
+        let bullets = document.createElement('div')
+        bullets.classList.add('glide__bullets')
+        bullets.setAttribute('data-glide-el', 'controls[nav]')
+        for (let i = 0; i < slides.length; i++) {
+            let button = document.createElement('button')
+            button.classList.add('glide__bullet')
+            button.setAttribute('data-glide-dir', `=${i}`)
+            bullets.appendChild(button)
+        }
+
+
+        glide.appendChild(bullets)
         con.appendChild(glide)
         reviewsSection.appendChild(con)
         
@@ -361,7 +390,7 @@ window.onload = (event) => {
         logos.appendChild(c)
 
         let heading = document.createElement('h2')
-        heading.innerHTML = 'Legal professionals are members of'
+        heading.innerHTML = 'All our legal professionals are accredited or regulated.'
         let subHeading = document.createElement('p')
         subHeading.innerHTML = 'We cover a wide vaiety of legal services'
 
@@ -371,45 +400,178 @@ window.onload = (event) => {
         let logosDiv = document.createElement('div')
         logosDiv.id = 'logosDiv'
 
-
-
-
+        let images = [
+            'https://via.placeholder.com/200',
+            'https://via.placeholder.com/200',
+            'https://via.placeholder.com/200',
+            'https://via.placeholder.com/200',
+        ]
+        images.forEach(image => {
+            let x = document.createElement('img')
+            x.src = image
+            logosDiv.appendChild(x)
+        })
+        c.appendChild(logosDiv)
 
         return logos
     }
 
-    function buildNesting(data){
-        let topEl = null
-        let myEls = []
+    function createTopFooter() {
+        let topFooter = document.createElement('section')
+        topFooter.id = 'topFooter'
 
-        data.forEach((item, i) => {
-            let x = document.createElement(item.el)
-            x.classList.add(item.classname)
-            if(item.sib) x.classList.add('sib')
-            myEls.push(x)
-        })
+        let c = createContainer()
+        topFooter.appendChild(c)
 
-        for (let i = 0; i < myEls.length; i++) {
-            if(i === 0) {
-                topEl = myEls[i]
-                continue
+        let h = document.createElement('h2')
+        h.innerHTML = 'Haven\’t found what you\’re looking for? We compare over 130 legal services.'
+        let p = document.createElement('p')
+        p.innerHTML = 'All advices are listed below'
+
+        c.appendChild(h)
+        c.appendChild(p)
+
+        c.appendChild(createRow({
+            title: '<i class="fa-solid fa-house"></i> Property',
+            col1:   [
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+            col2:   [
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+            col3:   [
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+        }))
+        c.appendChild(createRow({
+            title: '<i class="fa-solid fa-family"></i> Family',
+            col1:   [
+                "<a href='/'>link textt</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+            col2:   [
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+            col3:   [
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+        }))
+        c.appendChild(createRow({
+            title: 'Wills & Probate',
+            col1:   [
+                "<a href='/'>wills</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+            col2:   [
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+            col3:   [
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+                "<a href='/'>link text</a>",
+            ],
+        }))
+
+
+        function createRow(data) {
+            this.title = data.title
+            this.col1  = data.col1
+            this.col2  = data.col2
+            this.col3  = data.col3
+
+            let row = document.createElement('div')
+            row.classList.add('row')
+
+            makeCol([], this.title)
+            makeCol(this.col1)
+            makeCol(this.col2)
+            makeCol(this.col3)
+
+            //need 4 cols
+            function makeCol(c = [], title = null){
+                let col = document.createElement('div')
+                col.classList.add('col-12', 'col-md-3')
+
+                if(title) {
+                    col.innerHTML = `<h3 onclick='expandCat(this)'>${title}</h3>`
+                } else {
+                    c.forEach(item => {
+                        col.innerHTML += item
+                    })
+                }
+                row.appendChild(col)
             }
-            if(myEls[i].classList.contains('sib')) {
-                myEls[i - 2].appendChild(myEls[i])
-                myEls[i].classList.remove('sib')
-            } else {
-                myEls[i - 1].appendChild(myEls[i])
-            }
+
+            return row
         }
-        return topEl
+
+        return topFooter
     }
 
+    
+
+    // function buildNesting(data){
+    //     let topEl = null
+    //     let myEls = []
+
+    //     data.forEach((item, i) => {
+    //         let x = document.createElement(item.el)
+    //         x.classList.add(item.classname)
+    //         if(item.sib) x.classList.add('sib')
+    //         myEls.push(x)
+    //     })
+
+    //     for (let i = 0; i < myEls.length; i++) {
+    //         if(i === 0) {
+    //             topEl = myEls[i]
+    //             continue
+    //         }
+    //         if(myEls[i].classList.contains('sib')) {
+    //             myEls[i - 2].appendChild(myEls[i])
+    //             myEls[i].classList.remove('sib')
+    //         } else {
+    //             myEls[i - 1].appendChild(myEls[i])
+    //         }
+    //     }
+    //     return topEl
+    // }
+
     function removeExsiting() {
-        document.querySelector('.slideshow').remove()
+        //document.querySelector('.slideshow').remove()
     }
 
     //don't include in production
-    console.log(document.querySelector('head').innerHTML += '<style>.tls-cookie-popup {display: none !important;}</style>')
+    console.log(document.querySelector('head').innerHTML += '<style>.tls-cookie-popup {display: none !important;}.footer{display:none;}</style>')
 
     function createContainer() {
         let c = document.createElement('div')
@@ -421,3 +583,13 @@ window.onload = (event) => {
     
 };
 
+function expandCat(el) {
+    console.log(el)
+    let x = el.parentElement.parentElement
+    let cols = x.getElementsByClassName('col-12')
+    console.log(cols)
+    for (let i = 0; i < cols.length; i++) {
+        if(i === 0) continue
+        cols[i].classList.toggle('show')
+    }
+}
